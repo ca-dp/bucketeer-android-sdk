c7d2da35fd45515746f81f4608734f2d2056d7f4
yshrsmz <the.phantom.bane+github@gmail.com>
2022-09-12 17:39:35 +0900

    add more BKTClientImplTest

--- bucketeer2/src/test/kotlin/io/bucketeer/sdk/android/BKTClientImplTest.kt
+++ bucketeer2/src/test/kotlin/io/bucketeer/sdk/android/BKTClientImplTest.kt
@@ -10,6 +10,8 @@ import io.bucketeer.sdk.android.internal.model.EventData
 import io.bucketeer.sdk.android.internal.model.EventType
 import io.bucketeer.sdk.android.internal.model.MetricsEventData
 import io.bucketeer.sdk.android.internal.model.MetricsEventType
+import io.bucketeer.sdk.android.internal.model.User
+import io.bucketeer.sdk.android.internal.model.UserEvaluations
 import io.bucketeer.sdk.android.internal.model.request.RegisterEventsRequest
 import io.bucketeer.sdk.android.internal.model.response.ErrorResponse
 import io.bucketeer.sdk.android.internal.model.response.GetEvaluationsDataResponse
@@ -17,6 +19,7 @@ import io.bucketeer.sdk.android.internal.model.response.GetEvaluationsResponse
 import io.bucketeer.sdk.android.internal.model.response.RegisterEventsDataResponse
 import io.bucketeer.sdk.android.internal.model.response.RegisterEventsResponse
 import io.bucketeer.sdk.android.internal.user.toBKTUser
+import io.bucketeer.sdk.android.mocks.evaluation1
 import io.bucketeer.sdk.android.mocks.user1
 import io.bucketeer.sdk.android.mocks.user1Evaluations
 import okhttp3.mockwebserver.MockResponse
@@ -376,6 +379,270 @@ class BKTClientImplTest {
       ),
     )
   }
+
+  @Test
+  fun track() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = user1Evaluations,
+                  user_evaluations_id = "user_evaluations_id_value",
+                ),
+              ),
+            ),
+        ),
+    )
+
+    val initializeFuture = BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+    initializeFuture.get()
+
+    val client = BKTClient.getInstance() as BKTClientImpl
+    client.track("goal_id_value", 0.4)
+
+    Thread.sleep(100)
+
+    val actualEvents = client.component.dataModule.eventDao.getEvents()
+
+    assertThat(actualEvents).hasSize(3)
+    val goalEvent = actualEvents.last()
+
+    assertGoalEvent(goalEvent, "goal_id_value", 0.4, user1, config.featureTag)
+  }
+
+  @Test
+  fun evaluationDetails() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = user1Evaluations,
+                  user_evaluations_id = "user_evaluations_id_value",
+                ),
+              ),
+            ),
+        ),
+    )
+
+    val initializeFuture = BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+    initializeFuture.get()
+
+    val actual = BKTClient.getInstance().evaluationDetails(evaluation1.feature_id)
+
+    assertThat(actual).isEqualTo(
+      BKTEvaluation(
+        id = evaluation1.id,
+        featureId = evaluation1.feature_id,
+        featureVersion = evaluation1.feature_version,
+        userId = evaluation1.user_id,
+        variationId = evaluation1.variation_id,
+        variationValue = evaluation1.variation_value,
+        reason = evaluation1.reason.type.value,
+      ),
+    )
+  }
+
+  @Test
+  fun `evaluationDetails - unknown feature_id`() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = user1Evaluations,
+                  user_evaluations_id = "user_evaluations_id_value",
+                ),
+              ),
+            ),
+        ),
+    )
+
+    val initializeFuture = BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+    initializeFuture.get()
+
+    val actual = BKTClient.getInstance().evaluationDetails("unknown_feature_id")
+
+    assertThat(actual).isNull()
+  }
+
+  @Test
+  fun `fetchEvaluations - success`() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = UserEvaluations(
+                    id = "id_value",
+                    evaluations = listOf(evaluation1),
+                  ),
+                  user_evaluations_id = "user_evaluations_id_value",
+                ),
+              ),
+            ),
+        ),
+    )
+    val updatedEvaluation1 = evaluation1.copy(
+      variation_value = "test variation value1 updated",
+      variation = evaluation1.variation.copy(
+        value = "test variation value1 updated",
+      ),
+    )
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = user1Evaluations.copy(
+                    evaluations = listOf(updatedEvaluation1),
+                  ),
+                  user_evaluations_id = "user_evaluations_id_value_updated",
+                ),
+              ),
+            ),
+        ),
+    )
+
+    val initializeFuture = BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+    initializeFuture.get()
+
+    val client = BKTClient.getInstance() as BKTClientImpl
+    val result = client.fetchEvaluations().get()
+
+    assertThat(result).isNull()
+
+    assertThat(client.component.evaluationInteractor.currentEvaluationsId)
+      .isEqualTo("user_evaluations_id_value_updated")
+
+    assertThat(client.component.dataModule.evaluationDao.get(user1.id))
+      .isEqualTo(listOf(updatedEvaluation1))
+
+    assertThat(client.component.dataModule.eventDao.getEvents()).hasSize(4)
+  }
+
+  @Test
+  fun `fetchEvaluations - failure`() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(GetEvaluationsResponse::class.java)
+            .toJson(
+              GetEvaluationsResponse(
+                GetEvaluationsDataResponse(
+                  evaluations = user1Evaluations,
+                  user_evaluations_id = "user_evaluations_id_value",
+                ),
+              ),
+            ),
+        ),
+    )
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(500)
+        .setBody(
+          moshi.adapter(ErrorResponse::class.java)
+            .toJson(ErrorResponse(ErrorResponse.ErrorDetail(code = 500, message = "500 error"))),
+        ),
+    )
+
+    val initializeFuture = BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+    initializeFuture.get()
+
+    val client = BKTClient.getInstance() as BKTClientImpl
+    val result = client.fetchEvaluations().get()
+
+    assertThat(result).isInstanceOf(BKTException.ApiServerException::class.java)
+
+    assertThat(client.component.evaluationInteractor.currentEvaluationsId)
+      .isEqualTo("user_evaluations_id_value")
+
+    assertThat(client.component.dataModule.evaluationDao.get(user1.id)).hasSize(2)
+
+    val actualEvents = client.component.dataModule.eventDao.getEvents()
+    assertThat(actualEvents).hasSize(3)
+
+    val lastEvent = actualEvents.last()
+    assertThat(lastEvent.type).isEqualTo(EventType.METRICS)
+    assertThat((lastEvent.event as EventData.MetricsEvent).type)
+      .isEqualTo(MetricsEventType.INTERNAL_ERROR_COUNT)
+  }
+
+  @Test
+  fun currentUser() {
+    BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+
+    val actual = BKTClient.getInstance().currentUser()
+
+    assertThat(actual).isEqualTo(user1.toBKTUser())
+  }
+
+  @Test
+  fun setUserAttributes() {
+    BKTClient.initialize(
+      ApplicationProvider.getApplicationContext(),
+      config,
+      user1.toBKTUser(),
+      1000,
+    )
+
+    val attributes = mapOf(
+      "custom_key" to "custom_key_value",
+      "custom_key_2" to "custom_key_value_2",
+    )
+
+    BKTClient.getInstance().setUserAttributes(attributes)
+
+    assertThat(BKTClient.getInstance().currentUser())
+      .isEqualTo(user1.toBKTUser().copy(attributes = attributes))
+  }
 }
 
 // these assertion methods do not check full-equality, but that should be covered in other tests
@@ -426,4 +693,20 @@ fun assertTimeoutErrorCountMetricsEvent(
   val actualMetricsEvent = actual.event as EventData.MetricsEvent
   assertThat(actualMetricsEvent.type).isEqualTo(MetricsEventType.TIMEOUT_ERROR_COUNT)
   assertThat(actualMetricsEvent.event).isEqualTo(expectedMetricsEvent)
+}
+
+fun assertGoalEvent(
+  actual: Event,
+  expectedGoalId: String,
+  expectedValue: Double,
+  expectedUser: User,
+  expectedFeatureTag: String,
+) {
+  assertThat(actual.type).isEqualTo(EventType.GOAL)
+  val actualEventData = actual.event as EventData.GoalEvent
+
+  assertThat(actualEventData.goal_id).isEqualTo(expectedGoalId)
+  assertThat(actualEventData.value).isEqualTo(expectedValue)
+  assertThat(actualEventData.user).isEqualTo(expectedUser)
+  assertThat(actualEventData.tag).isEqualTo(expectedFeatureTag)
 }
