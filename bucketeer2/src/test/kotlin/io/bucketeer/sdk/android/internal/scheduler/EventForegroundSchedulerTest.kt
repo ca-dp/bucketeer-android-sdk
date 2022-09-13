14b4fa1f9074bf1454970e0a4095d88135bf6cd6
yshrsmz <the.phantom.bane+github@gmail.com>
2022-09-13 20:52:58 +0900

    add EventForegroundScheduler

---
+++ bucketeer2/src/test/kotlin/io/bucketeer/sdk/android/internal/scheduler/EventForegroundSchedulerTest.kt
@@ -0,0 +1,170 @@
+package io.bucketeer.sdk.android.internal.scheduler
+
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import com.squareup.moshi.Moshi
+import io.bucketeer.sdk.android.BKTConfig
+import io.bucketeer.sdk.android.internal.di.ComponentImpl
+import io.bucketeer.sdk.android.internal.di.DataModule
+import io.bucketeer.sdk.android.internal.di.InteractorModule
+import io.bucketeer.sdk.android.internal.model.EventType
+import io.bucketeer.sdk.android.internal.model.request.RegisterEventsRequest
+import io.bucketeer.sdk.android.internal.model.response.RegisterEventsDataResponse
+import io.bucketeer.sdk.android.internal.model.response.RegisterEventsResponse
+import io.bucketeer.sdk.android.internal.remote.measureTimeMillisWithResult
+import io.bucketeer.sdk.android.mocks.evaluation1
+import io.bucketeer.sdk.android.mocks.evaluation2
+import io.bucketeer.sdk.android.mocks.user1
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+@RunWith(RobolectricTestRunner::class)
+class EventForegroundSchedulerTest {
+  private lateinit var server: MockWebServer
+
+  private lateinit var component: ComponentImpl
+  private lateinit var moshi: Moshi
+  private lateinit var executor: ScheduledExecutorService
+  private lateinit var scheduler: EventForegroundScheduler
+
+  @Before
+  fun setup() {
+    server = MockWebServer()
+    component = ComponentImpl(
+      dataModule = DataModule(
+        application = ApplicationProvider.getApplicationContext(),
+        config = BKTConfig.builder()
+          .endpoint(server.url("").toString())
+          .apiKey("api_key_value")
+          .featureTag("feature_tag_value")
+          .eventsMaxQueueSize(3)
+          .eventsFlushInterval(1000)
+          .build(),
+        user = user1,
+        inMemoryDB = true,
+      ),
+      interactorModule = InteractorModule(),
+    )
+
+    moshi = component.dataModule.moshi
+
+    executor = Executors.newSingleThreadScheduledExecutor()
+
+    scheduler = EventForegroundScheduler(component, executor)
+  }
+
+  @After
+  fun tearDown() {
+    scheduler.stop()
+    server.shutdown()
+    executor.shutdownNow()
+  }
+
+  @Test
+  fun start() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(RegisterEventsResponse::class.java).toJson(
+            RegisterEventsResponse(RegisterEventsDataResponse(errors = emptyMap())),
+          ),
+        ),
+    )
+
+    scheduler.start()
+
+    assertThat(server.requestCount).isEqualTo(0)
+
+    component.eventInteractor.trackEvaluationEvent("feature_tag_value", user1, evaluation1)
+
+    val (time, _) = measureTimeMillisWithResult { server.takeRequest() }
+
+    assertThat(server.requestCount).isEqualTo(1)
+    assertThat(time).isGreaterThan(1000)
+  }
+
+  @Test
+  fun `sending via EventUpdateListener should reschedule`() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(RegisterEventsResponse::class.java).toJson(
+            RegisterEventsResponse(RegisterEventsDataResponse(errors = emptyMap())),
+          ),
+        ),
+    )
+
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(RegisterEventsResponse::class.java).toJson(
+            RegisterEventsResponse(RegisterEventsDataResponse(errors = emptyMap())),
+          ),
+        ),
+    )
+
+    scheduler.start()
+    assertThat(scheduler.isStarted).isTrue()
+
+    component.eventInteractor.trackEvaluationEvent("feature_tag_value", user1, evaluation1)
+    component.eventInteractor.trackEvaluationEvent("feature_tag_value", user1, evaluation2)
+
+    assertThat(server.requestCount).isEqualTo(0)
+
+    // 3rd event should trigger request and reschedule next flushing
+    component.eventInteractor.trackGoalEvent("feature_tag_value", user1, "goal_id_value", 0.4)
+
+    val (time, request) = measureTimeMillisWithResult { server.takeRequest() }
+    val requestBody = requireNotNull(
+      moshi.adapter(RegisterEventsRequest::class.java)
+        .fromJson(request.body.readString(Charsets.UTF_8)),
+    )
+
+    assertThat(time).isLessThan(100)
+
+    assertThat(requestBody.events.map { it.type })
+      .isEqualTo(listOf(EventType.EVALUATION, EventType.EVALUATION, EventType.GOAL))
+
+    component.eventInteractor.trackGoalEvent("feature_tag_value", user1, "goal_id_value", 0.5)
+
+    val (time2, request2) = measureTimeMillisWithResult { server.takeRequest() }
+
+    val requestBody2 = requireNotNull(
+      moshi.adapter(RegisterEventsRequest::class.java)
+        .fromJson(request2.body.readString(Charsets.UTF_8)),
+    )
+    assertThat(requestBody2.events).hasSize(1)
+    assertThat(time2).isGreaterThan(1000)
+  }
+
+  @Test
+  fun `stop should cancel scheduling`() {
+    server.enqueue(
+      MockResponse()
+        .setResponseCode(200)
+        .setBody(
+          moshi.adapter(RegisterEventsResponse::class.java).toJson(
+            RegisterEventsResponse(RegisterEventsDataResponse(errors = emptyMap())),
+          ),
+        ),
+    )
+
+    scheduler.start()
+    scheduler.stop()
+
+    val request = server.takeRequest(2, TimeUnit.SECONDS)
+    assertThat(request).isNull()
+    assertThat(server.requestCount).isEqualTo(0)
+  }
+}
